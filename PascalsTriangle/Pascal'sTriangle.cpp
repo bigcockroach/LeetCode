40031cdf13ace4f68e84dc20b467796fd4cca851
lichaolei <igis_lcl@fiberhome.com>
2016-03-08 15:55:16 +0800

    Pascal's Triangle

---
+++ PascalsTriangle/Pascal'sTriangle.cpp
@@ -0,0 +1,22 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<vector<int>> generate(int numRows) {
+		vector<vector<int>> result;
+		if (numRows == 0)
+			return result;
+		for (int i = 0; i < numRows; i++) {
+			vector<int> tmp;
+			for (int j = 0; j < i + 1; j++) {
+				if (j == 0 || j == i)
+					tmp.push_back(1);
+				else
+					tmp.push_back(result[i - 1][j - 1] + result[i - 1][j]);
+			}
+			result.push_back(tmp);
+		}
+		return result;
+	}
+};
