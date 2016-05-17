d74a3e5e5d2bccc21312d4bfbd73d4aca66e359b
lichaolei <igis_lcl@fiberhome.com>
2016-05-17 19:23:51 +0800

    First Bad Version

---
+++ FirstBadVersion/FirstBadVersion.cpp
@@ -0,0 +1,18 @@
+bool isBadVersion(int version);
+
+class Solution {
+public:
+	int firstBadVersion(int n) {
+		if (n <= 1)
+			return n;
+		int first = 1, last = n, mid;
+		while (first < last) {
+			mid = first + (last - first) / 2;
+			if (isBadVersion(mid))
+				last = mid;
+			else
+				first = mid + 1;
+		}
+		return first;
+	}
+};
