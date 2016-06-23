77cc5548b579410afbbfba4fb762bad8910852f0
lichaolei <841284086@qq.com>
2016-06-23 20:11:28 +0800

    Sort Colors

---
+++ SortColors/SortColors.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        red = 0
+        white = 0
+        blue = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                red += 1
+            elif nums[i] == 1:
+                white += 1
+            elif nums[i] == 2:
+                blue += 1
+        for i in range(red):
+            nums[i] = 0
+        for i in range(red, red + white):
+            nums[i] = 1
+        for i in range(red + white, len(nums)):
+            nums[i] = 2
