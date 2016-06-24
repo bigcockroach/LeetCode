0153a931a1ca653d1089df5bbab0ed43c147c1eb
lichaolei <841284086@qq.com>
2016-06-24 18:26:11 +0800

    Search a 2D Matrix

---
+++ SearchA2DMatrix/II/SearchA2DMatrixII.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return False
+        i = 0
+        j = len(matrix[0]) - 1
+    
+        while i < len(matrix) and j >= 0:
+            if target == matrix[i][j]:
+                return True
+            elif target > matrix[i][j]:
+                i += 1
+            else:
+                j -= 1
+        return False
