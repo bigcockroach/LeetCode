78cb6ed26a01c04b974729d4b4874d1cd35984c1
lichaolei <841284086@qq.com>
2016-07-12 11:17:31 +0800

    Super Pow

---
+++ SuperPow/SuperPow.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def __init__(self):
+    	self.base = 1337
+
+    def superPow(self, a, b):
+        """
+        :type a: int
+        :type b: List[int]
+        :rtype: int
+        """
+        if len(b) == 0:
+        	return 1
+        last_digit = b[-1]
+        b.pop()
+        return self.powmod(self.superPow(a, b), 10) * self.powmod(a, last_digit) % self.base
+
+    def powmod(self, a, k):
+    	a %= self.base
+    	result = 1
+    	for i in xrange(k):
+    		result = (result * a) % self.base
+    	return result
